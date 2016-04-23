ed75a1508fae8f636856528bcffea5c6c2f9c005
jaimecastignani <jaimecastignani@gmail.com>
2016-04-23 15:25:11 +0200

    info

--- js/index.js
+++ js/index.js
@@ -91,13 +91,46 @@ var app = {
         );
         var marker = new google.maps.Marker({
             position: latLng,
-            map: mapa
+            map: mapa,
+            title: 'Uluru (Ayers Rock)'
         });
+
+        var contentString = '<div id="content">' +
+            '<div id="siteNotice">' +
+            '</div>' +
+            '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+            '<div id="bodyContent">' +
+            '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+            'sandstone rock formation in the southern part of the ' +
+            'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
+            'south west of the nearest large town, Alice Springs; 450&#160;km ' +
+            '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
+            'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
+            'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
+            'Aboriginal people of the area. It has many springs, waterholes, ' +
+            'rock caves and ancient paintings. Uluru is listed as a World ' +
+            'Heritage Site.</p>' +
+            '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+            'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
+            '(last visited June 22, 2009).</p>' +
+            '</div>' +
+            '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+
+        marker.addListener('click', function () {
+            var element = document.getElementById('info');
+            element.innerHTML = "<img src=img/logo.png width=360 height=150>";
+            infowindow.open(mapa, marker);
+        });
+
         for (var x = 0; x < list.length; x++) {
             var elem = list[x].split(',');
             myLatLng.lat = parseFloat(elem[0]);
             myLatLng.lng = parseFloat(elem[1])
-            console.log(myLatLng);
+
             marker = new google.maps.Marker({
                 position: myLatLng,
                 map: mapa
